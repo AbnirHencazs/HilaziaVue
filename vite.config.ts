257b7ed210e6b9b387a7f2406ded9d1c3867aa77
Brian Sánchez <brian.sanchez@inmediatum.com>
2025-03-21 15:42:42 -0600

    remove tailwindcss module and config

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,11 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-import tailwindcss from '@tailwindcss/vite'
 import vuetify from 'vite-plugin-vuetify'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(), vue(), vueDevTools(), vuetify()],
+  plugins: [vue(), vueDevTools(), vuetify()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
