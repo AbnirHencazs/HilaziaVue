06447708d414e975203705b22a00194ad149f08b
Brian Sánchez <brian.sanchez@inmediatum.com>
2025-03-21 17:14:39 -0600

    Es un inicio, puse el sitio en modo construccion

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,14 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { RouterView } from 'vue-router'
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-  <div class="text-red-500">Tailwind works!!</div>
-  <RouterView />
+  <v-app>
+    <v-app-bar title="Hilazia"></v-app-bar>
+    <v-main>
+      <RouterView />
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,22 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import HilaziaIcon from '@/components/icons/HilaziaIcon.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <v-container class="fill-height flex-column">
+    <v-row justify="center">
+      <v-col>
+        <v-sheet>
+          <h1>Muy pronto</h1>
+        </v-sheet>
+      </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-col>
+        <v-sheet>
+          <hilazia-icon></hilazia-icon>
+        </v-sheet>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
