29b8e3e14c338a441d107fc4e8f62060425d84ed
Branchez <brian.sanchez@homegauge.com>
2025-03-24 17:11:43 -0600

    Implement a color theme and navigation

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,3 +1,23 @@
-import { createVuetify } from 'vuetify'
+import { createVuetify, type ThemeDefinition } from 'vuetify'
 
-export default createVuetify()
+const hilazia: ThemeDefinition = {
+  colors: {
+    primary: '#70908B',
+    secondary: '#CAF3E5',
+    'primary-darken-1': '#07484A',
+    error: '#FFF4E7',
+    info: '#E0EFF6',
+    success: '#CAF3E5',
+    warning: '#EEEBFF',
+  }
+}
+
+
+export default createVuetify({
+  theme: {
+    defaultTheme: 'hilazia',
+    themes: {
+      hilazia
+    }
+  }
+})
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,7 +6,7 @@ import HilaziaIcon from '@/components/icons/HilaziaIcon.vue'
   <v-container class="fill-height flex-column">
     <v-row justify="center">
       <v-col>
-        <v-sheet class="text-h1"> Muy Pronto... </v-sheet>
+        <h1 class="text-h1">Muy Pronto...</h1>
       </v-col>
     </v-row>
     <v-row justify="center">
@@ -17,4 +17,19 @@ import HilaziaIcon from '@/components/icons/HilaziaIcon.vue'
       </v-col>
     </v-row>
   </v-container>
+  <v-container>
+    <v-row>
+      <v-col>
+        <h1 class="text-h1">
+          Explorar por categoria
+        </h1>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
+
+<style>
+  .text-h1 {
+    color: rgb(var(--v-theme-primary-darken-1));
+  }
+</style>
