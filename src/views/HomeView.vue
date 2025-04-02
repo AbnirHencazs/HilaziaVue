b2418ebfcbcd58253817c5aadcdbc2e58a777235
Brian Sánchez <brian.sanchez@inmediatum.com>
2025-04-02 16:45:01 -0600

    Cambiar el Tagline

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,9 @@
 <template>
   <v-parallax class="rounded-b-16" src="http://hilazia.online/images/HilaziaHero.webp">
     <v-container class="fill-height flex-column align-start">
-      <v-row class="align-end"><h1 class="text-h1">Lorem Ipsum Dolor</h1></v-row>
+      <v-row class="align-end"
+        ><h1 class="text-h1">Porque lo auténtico nunca pasa de moda.</h1></v-row
+      >
       <v-row><v-btn color="primary">Shop</v-btn></v-row>
     </v-container>
   </v-parallax>
@@ -15,26 +17,26 @@
   <v-container>
     <v-row class="h-25">
       <v-col>
-        <v-sheet class="px-2 py-8" rounded color="primary">Macrame</v-sheet>
+        <v-card class="px-2 py-8 text-center" rounded color="primary">Macrame</v-card>
       </v-col>
       <v-col>
-        <v-sheet class="px-2 py-8" rounded color="primary">Kombuchas</v-sheet>
-      </v-col>
-    </v-row>
-    <v-row class="h-25">
-      <v-col>
-        <v-sheet class="px-2 py-8" rounded color="primary">Corsets</v-sheet>
-      </v-col>
-      <v-col>
-        <v-sheet class="px-2 py-8" rounded color="primary">Vestidos</v-sheet>
+        <v-card class="px-2 py-8 text-center" rounded color="primary">Kombuchas</v-card>
       </v-col>
     </v-row>
     <v-row class="h-25">
       <v-col>
-        <v-sheet class="px-2 py-8" rounded color="primary">Plantas</v-sheet>
+        <v-card class="px-2 py-8 text-center" rounded color="primary">Corsets</v-card>
       </v-col>
       <v-col>
-        <v-sheet class="px-2 py-8" rounded color="primary">Pantalones</v-sheet>
+        <v-card class="px-2 py-8 text-center" rounded color="primary">Vestidos</v-card>
+      </v-col>
+    </v-row>
+    <v-row class="h-25">
+      <v-col>
+        <v-card class="px-2 py-8 text-center" rounded color="primary">Plantas</v-card>
+      </v-col>
+      <v-col>
+        <v-card class="px-2 py-8 text-center" rounded color="primary">Pantalones</v-card>
       </v-col>
     </v-row>
   </v-container>
