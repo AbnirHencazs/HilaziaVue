196b1d9a998088a9769f4776444cb127bafe70cf
Brian Sánchez <brian.sanchez@inmediatum.com>
2025-04-07 17:49:37 -0500

    Agregue SEO a la aplicacion

---
+++ src/composables/useMeta.ts
@@ -0,0 +1,98 @@
+import { useHead } from '@unhead/vue'
+
+interface MetaOptions {
+  title: string
+  description?: string
+  image?: string
+  url?: string
+  keywords?: string
+  author?: string
+  themeColor?: string
+}
+
+export function useMeta(options: MetaOptions) {
+  const {
+    title,
+    description = 'Hilazia - Arte que se siente, que se viste y que se prueba',
+    image = 'https://hilazia.online/images/HilaziaHero.webp',
+    url = 'https://hilazia.online',
+    keywords = 'hilazia, ropa, moda, arte, diseño, tienda online',
+    author = 'Hilazia',
+    themeColor = '#ffffff',
+  } = options
+
+  useHead({
+    title: `${title} | Hilazia`,
+    meta: [
+      {
+        name: 'description',
+        content: description,
+      },
+      {
+        name: 'keywords',
+        content: keywords,
+      },
+      {
+        name: 'author',
+        content: author,
+      },
+      {
+        name: 'theme-color',
+        content: themeColor,
+      },
+      // Open Graph / Facebook
+      {
+        property: 'og:type',
+        content: 'website',
+      },
+      {
+        property: 'og:url',
+        content: url,
+      },
+      {
+        property: 'og:title',
+        content: title,
+      },
+      {
+        property: 'og:description',
+        content: description,
+      },
+      {
+        property: 'og:image',
+        content: image,
+      },
+      // Twitter
+      {
+        property: 'twitter:card',
+        content: 'summary_large_image',
+      },
+      {
+        property: 'twitter:url',
+        content: url,
+      },
+      {
+        property: 'twitter:title',
+        content: title,
+      },
+      {
+        property: 'twitter:description',
+        content: description,
+      },
+      {
+        property: 'twitter:image',
+        content: image,
+      },
+    ],
+    link: [
+      {
+        rel: 'canonical',
+        href: url,
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        href: '/favicon.png',
+      },
+    ],
+  })
+}
--- src/main.ts
+++ src/main.ts
@@ -3,15 +3,28 @@ import 'vuetify/styles'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import { createHead } from '@unhead/vue/client'
 
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
 
 const app = createApp(App)
+const head = createHead()
 
 app.use(createPinia())
 app.use(router)
 app.use(vuetify)
+app.use(head)
 
+// Add default meta tags
+head.push({
+  title: 'Hilazia - Online Store',
+  meta: [
+    {
+      name: 'description',
+      content: 'Tienda de kombucha, macramé, plantas y moda en Puebla, México',
+    },
+  ],
+})
 app.mount('#app')
